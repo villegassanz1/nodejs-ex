72e73b739bd6688f59def9d889134564b5b41413
rodrigo ramos candela <rodrigo ramos candela>
2016-12-17 02:38:28 -0600

    actualizacion8

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 var express = require('express');  
 var app = express();  
-var server = require('http').Server(app);  
-
-var messages = [
-
+var server = require('http').Server(app);
+var mongoose = require('mongoose');
+mongoose.connect('mongodb: //localhost/test');  
+/*
+var developer = [
+	{first: 'Alice', last: 'Wonderland'},
+	{first: 'Bob', last: 'Marley'}
 ];
-
+*/
 app.use(express.static('public'));
 
 app.get('/', function(request,response){
   response.sendFile(__dirname + '/index.html');
 });
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
+app.listen(port, ip);
+/*
 server.listen(8080, function() {  
   console.log("Servidor corriendo en http://localhost:8080");
 });
+*/
