3e7a5a20a6c96cb41ef91c5aea752a094e7a84d9
rodrigo ramos candela <rodrigo ramos candela>
2016-12-17 02:46:56 -0600

    actualizacion01

--- server.js
+++ server.js
@@ -14,12 +14,11 @@ app.use(express.static('public'));
 app.get('/', function(request,response){
   response.sendFile(__dirname + '/index.html');
 });
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+//var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+//var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.listen(port, ip);
-/*
+//app.listen(port, ip);
+
 server.listen(8080, function() {  
   console.log("Servidor corriendo en http://localhost:8080");
 });
-*/
