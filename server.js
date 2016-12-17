4d6244bf9186bd0ca2e6d0ef6b08662899dae283
rodrigo ramos candela <rodrigo ramos candela>
2016-12-17 08:36:03 -0600

    actualizateaaaazxcvxc

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.get('/', function(request,response){
   response.sendFile(__dirname + '/index.html');
 });
 
-app.get('/api/usuario', function(req, res){
-	var user = new UsuarioModel({_id : req.param('id'), nombre: req.param('nombre') });
+app.get('/api/usuario/:id/:name', function(req, res){
+	var user = new UsuarioModel({_id : req.params.id, nombre: req.params.name });
 	user.save(function(err,doc){
 		res.json(doc);	
 	});
