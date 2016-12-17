6827eb84da74e31a5b3dbb52bad9e2d119b8ef39
rodrigo ramos candela <rodrigo ramos candela>
2016-12-17 07:13:34 -0600

    actualizateaaaazx

--- server.js
+++ server.js
@@ -7,28 +7,46 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://villegas:12345@172.30.202.200:27017/buslocation');  
 
-var WebSiteSchema = new mongoose.Schema({
-	name: String,
-	created : {type : Date, default: Date.now}
-}, {collection : "website"});
+	var UsuarioSchema = new mongoose.Schema({
+		_id: String,
+		nombre : String,
+		latitud : String,
+		longitud : String,
+		created : {type : Date, default: Date.now}
+	}, {collection : "usuario"});
 
-var WebSiteModel = mongoose.model('WebSite', WebSiteSchema);
-/*
-var developer = [
-	{first: 'Alice', last: 'Wonderland'},
-	{first: 'Bob', last: 'Marley'}
-];
-*/
+	var UsuarioModel = mongoose.model('usuario', UsuarioSchema);
+
+	var WebSiteSchema = new mongoose.Schema({
+		name: String,
+		created : {type : Date, default: Date.now}
+	}, {collection : "website"});
+
+	var WebSiteModel = mongoose.model('WebSite', WebSiteSchema);
+
 app.use(express.static('public'));
 
 app.get('/', function(request,response){
   response.sendFile(__dirname + '/index.html');
 });
 
+app.get('/api/usuario/:id', function(req, res){
+	var user = new UsuarioModel({_id : req.params.id });
+	user.save(function(err,doc){
+		res.json(doc);	
+	});
+});
+
 app.get('/api/website/:name', function(req, res){
 	var website = new WebSiteModel({name : req.params.name });
 	website.save(function(err,doc){
 		res.json(doc);	
+	});
+});
+
+app.get('/api/usuarios',function(req, res){
+	UsuarioModel.find(function(err, sites){
+		res.json(sites);
 	});
 });
 
